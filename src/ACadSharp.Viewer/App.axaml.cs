a404564bc92d427525c3d6b5b0284e28a6757232
Wiesław Šoltés <wieslaw.soltes@gmail.com>
2025-07-31 00:05:24 +0200

    Add dev tools

--- src/ACadSharp.Viewer/App.axaml.cs
+++ src/ACadSharp.Viewer/App.axaml.cs
@@ -12,6 +12,7 @@ public partial class App : Application
     public override void Initialize()
     {
         AvaloniaXamlLoader.Load(this);
+        this.AttachDevTools();
     }
 
     public override void OnFrameworkInitializationCompleted()
